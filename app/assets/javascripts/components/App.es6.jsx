2694f0b14513fc13bb5e8ca942e49703bb8f08ac
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-21 15:23:16 -0400

    Refactor to move all variables to the app component

--- app/assets/javascripts/components/App.es6.jsx
+++ app/assets/javascripts/components/App.es6.jsx
@@ -4,11 +4,23 @@ class App extends React.Component {
       photoUrl: "https://vignette.wikia.nocookie.net/logopedia/images/a/a4/Google-Currents-Featured-300x300.png/revision/latest?cb=20120704191200",
       bio: "Hello I am adam"
     }
+    const photos = [
+      {
+      photo_url: "https://static.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg",
+      user_id: 1,
+      blurb: "Chuck Norris inherits from two classes #bro"
+      },
+      {
+      photo_url: "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
+      user_id: 2,
+      blurb: "Superman wears Chuck Norris pyjamas!"
+      }
+    ]
     return(
       <div id="container">
         <ProfileInfo profileInfo={profileInfo} />
         <NewPhotoForm />
-        <Timeline />
+        <Timeline photos={photos} />
       </div>
     )
   }
