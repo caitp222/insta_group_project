c62f241b153839e45d70354d835e75286d1f8004
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-21 14:32:43 -0400

    Add skeleton of Timeline class

---
+++ app/assets/javascripts/components/Timeline.es6.jsx
@@ -0,0 +1,21 @@
+class Timeline extends React.Component {
+  render() {
+    let photos = [
+      {
+      photo_url: "https://static.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg",
+      user_id: 1,
+      blurb: "Chuck Norris inherits from two classes #bro"
+      },
+      {
+      photo_url: "https://static.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg",
+      user_id: 1,
+      blurb: "Chuck Norris inherits from two classes #bro"
+      }
+    ]
+    return(
+      <div>
+        {}
+      </div>
+    )
+  }
+}
