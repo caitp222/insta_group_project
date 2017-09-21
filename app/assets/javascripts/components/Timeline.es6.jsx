f68e4400de9a0b892d220c7240de049a05b4a4a6
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-21 15:24:00 -0400

    Refactor

--- app/assets/javascripts/components/Timeline.es6.jsx
+++ app/assets/javascripts/components/Timeline.es6.jsx
@@ -1,17 +1,6 @@
 class Timeline extends React.Component {
   render() {
-    const photos = [
-      {
-      photo_url: "https://static.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg",
-      user_id: 1,
-      blurb: "Chuck Norris inherits from two classes #bro"
-      },
-      {
-      photo_url: "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
-      user_id: 2,
-      blurb: "Superman wears Chuck Norris pyjamas!"
-      }
-    ]
+    const photos = this.props.photos
     return(
       <div>
         {photos.map((photo, i) =>
