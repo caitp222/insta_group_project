871f316502abfbe9fe1593d37a921dbd0e1c0973
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-21 14:43:01 -0400

    add timeline and photo components

--- app/assets/javascripts/components/Timeline.es6.jsx
+++ app/assets/javascripts/components/Timeline.es6.jsx
@@ -1,20 +1,22 @@
 class Timeline extends React.Component {
   render() {
-    let photos = [
+    const photos = [
       {
       photo_url: "https://static.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg",
       user_id: 1,
       blurb: "Chuck Norris inherits from two classes #bro"
       },
       {
-      photo_url: "https://static.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg",
-      user_id: 1,
-      blurb: "Chuck Norris inherits from two classes #bro"
+      photo_url: "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
+      user_id: 2,
+      blurb: "Superman wears Chuck Norris pyjamas!"
       }
     ]
     return(
       <div>
-        {}
+        {photos.map((photo) =>
+          <Photo photo={photo} />
+        )}
       </div>
     )
   }
