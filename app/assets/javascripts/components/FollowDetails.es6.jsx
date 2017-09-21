b521427a4f93a2579e372190b2d7f3c16561fd79
Adam Weil <adamjweil@gmail.com>
2017-09-21 14:58:56 -0400

    adams components

---
+++ app/assets/javascripts/components/FollowDetails.es6.jsx
@@ -0,0 +1,14 @@
+class FollowDetails extends React.Component {
+  render(){
+
+    const { followers, following, photoCount } = this.props
+
+    return(
+      <div>
+        <hr />
+        <center><h4>Followers: {followers}  |  Following: {following}  |  Photos: {photoCount}</h4></center>
+        <hr />
+      </div>
+    )
+  }
+}
