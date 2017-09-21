8d1bf255a9cecf2c56a2d4e18283713b8e3dbce2
RicoDe Santis <ricodesantis@gmail.com>
2017-09-21 16:17:56 -0400

    paperclip migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :likes
 
+  has_attached_file :avatar, :styles => {:medium => "300x300 >", :thumb => "100x100 >"}
+  validates_attachement_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   # has_many :relationships, foreign_key: :following_id
   # has_many :followings, through: :relationships, source: :user
 
